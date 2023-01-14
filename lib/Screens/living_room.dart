ed936041bbb0bf1ec73c3e4a517bde15ea88cfcb
Nur Siyam <nur.siyam@gmail.com>
2023-01-14 14:41:53 +0400

    Line graph added

--- lib/Screens/living_room.dart
+++ lib/Screens/living_room.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_database/firebase_database.dart';
 import 'package:flutter/material.dart';
 
 class LivingRoom extends StatefulWidget {
@@ -12,6 +13,61 @@ class _LivingRoomState extends State<LivingRoom> {
   bool acOn = false;
   bool tvOn = false;
   bool curtainClosed = true;
+  int tempReading = 10;
+  int lightReading = 30;
+
+//Read from the database
+  dynamic data;
+  final dataBase = FirebaseDatabase.instance.ref();
+  @override
+  void initState() {
+    //Read the data from the database once
+    dataBase.child('ESP').once().then((snap) {
+      data = snap.snapshot.value;
+      lightReading = data['LDR'];
+      tempReading = data['temp'];
+      ledOn = data['LED'] == 1;
+    }).then((value) {
+      setState(() {});
+    });
+
+    //Listen to database for changes
+    dataBase.child('ESP').onChildChanged.listen((event) {
+      DataSnapshot snap = event.snapshot;
+
+      if (snap.key == 'LDR') {
+        setState(() {
+          lightReading = int.parse(snap.value.toString());
+        });
+      }
+
+      if (snap.key == 'temp') {
+        setState(() {
+          tempReading = int.parse(snap.value.toString());
+        });
+      }
+    });
+    super.initState();
+  }
+
+  //Function to toggle the switch and change the value on the database
+  void toggleSwitchLed(bool value) {
+    if (ledOn == false) {
+      setState(() {
+        ledOn = true;
+        final child = dataBase.child('ESP/');
+        int boolString = ledOn ? 1 : 0;
+        child.update({'LED': boolString});
+      });
+    } else {
+      setState(() {
+        ledOn = false;
+        final child = dataBase.child('ESP/');
+        int boolString = ledOn ? 1 : 0;
+        child.update({'LED': boolString});
+      });
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -20,163 +76,265 @@ class _LivingRoomState extends State<LivingRoom> {
           leading: const BackButton(),
           title: const Text('Living Room'),
         ),
-        body: GridView.count(
-          crossAxisCount: 2,
-          primary: false,
-          padding: const EdgeInsets.all(20),
-          crossAxisSpacing: 10,
-          mainAxisSpacing: 10,
-          children: [
-            Container(
-              decoration: BoxDecoration(
-                color: Colors.lightBlue[50],
-                borderRadius: BorderRadius.circular(20),
-              ),
-              padding: EdgeInsets.all(6),
-              margin: EdgeInsets.only(right: 6),
-              child: Column(
-                children: [
-                  SizedBox(height: 15),
-                  Row(
-                    children: [
-                      Icon(
-                        Icons.light,
-                        size: 60,
-                        color: Colors.grey,
+        body: Container(
+            decoration: BoxDecoration(
+                image: DecorationImage(
+                    image: NetworkImage('https://i.imgur.com/gdlnkvN.jpeg'),
+                    fit: BoxFit.cover)),
+            child: Column(
+              children: [
+                SizedBox(height: 15),
+                Container(
+                    decoration: BoxDecoration(
+                      color: Colors.lightBlue[50],
+                      borderRadius: BorderRadius.circular(20),
+                    ),
+                    padding: EdgeInsets.all(6),
+                    margin: EdgeInsets.only(right: 6),
+                    width: MediaQuery.of(context).size.width * 0.9,
+                    child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Column(
+                            children: [
+                              Text(
+                                'Temperature',
+                                style:
+                                    TextStyle(fontSize: 16, color: Colors.grey),
+                              ),
+                              Row(
+                                children: [
+                                  Icon(
+                                    Icons.thermostat,
+                                    size: 24,
+                                    color: Colors.black,
+                                  ),
+                                  Text(
+                                    '$tempReading',
+                                    style: TextStyle(
+                                        fontSize: 16, color: Colors.grey),
+                                  ),
+                                  Text(
+                                    ' C',
+                                    style: TextStyle(
+                                        fontSize: 16, color: Colors.grey),
+                                  ),
+                                ],
+                              )
+                            ],
+                          ),
+                          Column(
+                            children: [
+                              Text(
+                                'Brightness',
+                                style:
+                                    TextStyle(fontSize: 16, color: Colors.grey),
+                              ),
+                              Row(
+                                children: [
+                                  Icon(
+                                    Icons.water_drop_outlined,
+                                    size: 24,
+                                    color: Colors.black,
+                                  ),
+                                  Text(
+                                    '$lightReading',
+                                    style: TextStyle(
+                                        fontSize: 16, color: Colors.grey),
+                                  ),
+                                  Text(
+                                    ' %',
+                                    style: TextStyle(
+                                        fontSize: 16, color: Colors.grey),
+                                  ),
+                                ],
+                              ),
+                            ],
+                          ),
+                          Column(
+                            children: [
+                              Text(
+                                'Energy Used',
+                                style:
+                                    TextStyle(fontSize: 16, color: Colors.grey),
+                              ),
+                              Row(
+                                children: [
+                                  Icon(
+                                    Icons.bolt_sharp,
+                                    size: 14,
+                                    color: Colors.black,
+                                  ),
+                                  Text(
+                                    '250 KWh',
+                                    style: TextStyle(
+                                        fontSize: 16, color: Colors.grey),
+                                  ),
+                                ],
+                              )
+                            ],
+                          ),
+                        ])),
+                Expanded(
+                    child: GridView.count(
+                  crossAxisCount: 2,
+                  primary: false,
+                  padding: const EdgeInsets.all(20),
+                  crossAxisSpacing: 10,
+                  mainAxisSpacing: 10,
+                  children: [
+                    Container(
+                      decoration: BoxDecoration(
+                        color: Colors.lightBlue[50],
+                        borderRadius: BorderRadius.circular(20),
                       ),
-                      Switch.adaptive(
-                        activeColor: Colors.blue[900],
-                        value: ledOn,
-                        onChanged: (bool v) {
-                          setState(() {
-                            ledOn = v;
-                          });
-                        },
-                        materialTapTargetSize: MaterialTapTargetSize.shrinkWrap,
+                      padding: EdgeInsets.all(6),
+                      margin: EdgeInsets.only(right: 6),
+                      child: Column(
+                        children: [
+                          SizedBox(height: 15),
+                          Row(
+                            children: [
+                              Icon(
+                                Icons.light,
+                                size: 60,
+                                color: Colors.grey,
+                              ),
+                              Switch.adaptive(
+                                activeColor: Colors.blue[900],
+                                value: ledOn,
+                                onChanged: toggleSwitchLed,
+                                materialTapTargetSize:
+                                    MaterialTapTargetSize.shrinkWrap,
+                              ),
+                            ],
+                          ),
+                          SizedBox(height: 15),
+                          Text(
+                            'Lamp',
+                            style: TextStyle(fontSize: 26, color: Colors.grey),
+                          ),
+                        ],
                       ),
-                    ],
-                  ),
-                  SizedBox(height: 15),
-                  Text(
-                    'Lamp',
-                    style: TextStyle(fontSize: 26, color: Colors.grey),
-                  ),
-                ],
-              ),
-            ),
-            Container(
-              decoration: BoxDecoration(
-                color: Colors.lightBlue[50],
-                borderRadius: BorderRadius.circular(20),
-              ),
-              padding: EdgeInsets.all(6),
-              margin: EdgeInsets.only(right: 6),
-              child: Column(
-                children: [
-                  SizedBox(height: 15),
-                  Row(
-                    children: [
-                      Icon(
-                        Icons.ac_unit,
-                        size: 60,
-                        color: Colors.grey,
+                    ),
+                    Container(
+                      decoration: BoxDecoration(
+                        color: Colors.lightBlue[50],
+                        borderRadius: BorderRadius.circular(20),
                       ),
-                      Switch.adaptive(
-                        activeColor: Colors.blue[900],
-                        value: acOn,
-                        onChanged: (bool v) {
-                          setState(() {
-                            acOn = v;
-                          });
-                        },
-                        materialTapTargetSize: MaterialTapTargetSize.shrinkWrap,
+                      padding: EdgeInsets.all(6),
+                      margin: EdgeInsets.only(right: 6),
+                      child: Column(
+                        children: [
+                          SizedBox(height: 15),
+                          Row(
+                            children: [
+                              Icon(
+                                Icons.ac_unit,
+                                size: 60,
+                                color: Colors.grey,
+                              ),
+                              Switch.adaptive(
+                                activeColor: Colors.blue[900],
+                                value: acOn,
+                                onChanged: (bool v) {
+                                  setState(() {
+                                    acOn = v;
+                                  });
+                                },
+                                materialTapTargetSize:
+                                    MaterialTapTargetSize.shrinkWrap,
+                              ),
+                            ],
+                          ),
+                          SizedBox(height: 15),
+                          Text(
+                            'AC Unit',
+                            style: TextStyle(fontSize: 26, color: Colors.grey),
+                          ),
+                        ],
                       ),
-                    ],
-                  ),
-                  SizedBox(height: 15),
-                  Text(
-                    'AC Unit',
-                    style: TextStyle(fontSize: 26, color: Colors.grey),
-                  ),
-                ],
-              ),
-            ),
-            Container(
-              decoration: BoxDecoration(
-                color: Colors.lightBlue[50],
-                borderRadius: BorderRadius.circular(20),
-              ),
-              padding: EdgeInsets.all(6),
-              margin: EdgeInsets.only(right: 6),
-              child: Column(
-                children: [
-                  SizedBox(height: 15),
-                  Row(
-                    children: [
-                      Icon(
-                        Icons.tv_rounded,
-                        size: 60,
-                        color: Colors.grey,
+                    ),
+                    Container(
+                      decoration: BoxDecoration(
+                        color: Colors.lightBlue[50],
+                        borderRadius: BorderRadius.circular(20),
                       ),
-                      Switch.adaptive(
-                        activeColor: Colors.blue[900],
-                        value: tvOn,
-                        onChanged: (bool v) {
-                          setState(() {
-                            tvOn = v;
-                          });
-                        },
-                        materialTapTargetSize: MaterialTapTargetSize.shrinkWrap,
+                      padding: EdgeInsets.all(6),
+                      margin: EdgeInsets.only(right: 6),
+                      child: Column(
+                        children: [
+                          SizedBox(height: 15),
+                          Row(
+                            children: [
+                              Icon(
+                                Icons.tv_rounded,
+                                size: 60,
+                                color: Colors.grey,
+                              ),
+                              Switch.adaptive(
+                                activeColor: Colors.blue[900],
+                                value: tvOn,
+                                onChanged: (bool v) {
+                                  setState(() {
+                                    tvOn = v;
+                                  });
+                                },
+                                materialTapTargetSize:
+                                    MaterialTapTargetSize.shrinkWrap,
+                              ),
+                            ],
+                          ),
+                          SizedBox(height: 15),
+                          Text(
+                            'Smart TV',
+                            style: TextStyle(fontSize: 26, color: Colors.grey),
+                          ),
+                        ],
                       ),
-                    ],
-                  ),
-                  SizedBox(height: 15),
-                  Text(
-                    'Smart TV',
-                    style: TextStyle(fontSize: 26, color: Colors.grey),
-                  ),
-                ],
-              ),
-            ),
-            Container(
-              decoration: BoxDecoration(
-                color: Colors.lightBlue[50],
-                borderRadius: BorderRadius.circular(20),
-              ),
-              padding: EdgeInsets.all(6),
-              margin: EdgeInsets.only(right: 6),
-              child: Column(
-                children: [
-                  SizedBox(height: 15),
-                  Row(
-                    children: [
-                      Icon(
-                        Icons.curtains,
-                        size: 60,
-                        color: Colors.grey,
+                    ),
+                    Container(
+                      decoration: BoxDecoration(
+                        color: Colors.lightBlue[50],
+                        borderRadius: BorderRadius.circular(20),
                       ),
-                      Switch.adaptive(
-                        activeColor: Colors.blue[900],
-                        value: curtainClosed,
-                        onChanged: (bool v) {
-                          setState(() {
-                            curtainClosed = v;
-                          });
-                        },
-                        materialTapTargetSize: MaterialTapTargetSize.shrinkWrap,
+                      padding: EdgeInsets.all(6),
+                      margin: EdgeInsets.only(right: 6),
+                      child: Column(
+                        children: [
+                          SizedBox(height: 15),
+                          Row(
+                            children: [
+                              Icon(
+                                Icons.curtains,
+                                size: 60,
+                                color: Colors.grey,
+                              ),
+                              Switch.adaptive(
+                                activeColor: Colors.blue[900],
+                                value: curtainClosed,
+                                onChanged: (bool v) {
+                                  setState(() {
+                                    curtainClosed = v;
+                                  });
+                                },
+                                materialTapTargetSize:
+                                    MaterialTapTargetSize.shrinkWrap,
+                              ),
+                            ],
+                          ),
+                          SizedBox(height: 15),
+                          Text(
+                            'Curtains',
+                            style: TextStyle(fontSize: 26, color: Colors.grey),
+                          ),
+                        ],
                       ),
-                    ],
-                  ),
-                  SizedBox(height: 15),
-                  Text(
-                    'Curtains',
-                    style: TextStyle(fontSize: 26, color: Colors.grey),
-                  ),
-                ],
-              ),
-            ),
-          ],
-        ));
+                    ),
+                  ],
+                ))
+              ],
+            )));
   }
 }
 
--- lib/Screens/statisctics_screen.dart
+++ lib/Screens/statisctics_screen.dart
@@ -1,6 +1,10 @@
+import 'dart:convert';
+
+import 'package:draw_graph/draw_graph.dart';
+import 'package:draw_graph/models/feature.dart';
+import 'package:firebase_database/firebase_database.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/src/widgets/container.dart';
-import 'package:flutter/src/widgets/framework.dart';
+import 'package:http/http.dart' as http;
 
 class StatisticsScreen extends StatefulWidget {
   const StatisticsScreen({super.key});
@@ -10,8 +14,135 @@ class StatisticsScreen extends StatefulWidget {
 }
 
 class _StatisticsScreenState extends State<StatisticsScreen> {
+// Database code
+  bool ledOn = true;
+  bool acOn = false;
+  bool tvOn = false;
+  bool curtainClosed = true;
+  int tempReading = 10;
+  int lightReading = 30;
+  int minutes = 1;
+
+//Read from the database
+  dynamic data;
+  final dataBase = FirebaseDatabase.instance.ref();
   @override
+  void initState() {
+    //Read the data from the database once
+    dataBase.child('ESP').once().then((snap) {
+      data = snap.snapshot.value;
+      lightReading = data['LDR'];
+      tempReading = data['temp'];
+      ledOn = data['LED'] == 1;
+    }).then((value) {
+      setState(() {
+        getJson();
+      });
+    });
+
+    //Listen to database for changes
+    dataBase.child('ESP').onChildChanged.listen((event) {
+      DataSnapshot snap = event.snapshot;
+
+      if (snap.key == 'LDR') {
+        setState(() {
+          lightReading = int.parse(snap.value.toString());
+        });
+      }
+
+      if (snap.key == 'temp') {
+        setState(() {
+          tempReading = int.parse(snap.value.toString());
+        });
+      }
+      if (snap.key == 'minutes') {
+        setState(() {
+          minutes = int.parse(snap.value.toString());
+          getJson();
+        });
+      }
+    });
+    super.initState();
+  }
+
+// Json Code
+  late List<dynamic> ldrValues;
+  late String json;
+  int itemCount = 60;
+  List<double> ldrData = [];
+  List<String> time = [];
+
+  Future getJson() async {
+    var url = Uri.parse(
+        'https://script.google.com/macros/s/AKfycby3YCkkLzBKpOimOt6xs-JKF9BCJC6ZoHHORVxGrYOWd164cOx_HS4vvDTeovWgHEjYig/exec');
+    json = await http.read(url).then((value) {
+      ldrValues = jsonDecode(value);
+      itemCount = ldrValues.length < 60 ? ldrValues.length : 60;
+      var dataUsed = ldrValues.sublist(0, itemCount);
+      ldrData = [];
+      time = [];
+      dataUsed.forEach((element) {
+        ldrData.add((element['value'].toDouble()) / 100);
+        time.add(element['date'].split('T')[1].substring(0, 5));
+      });
+      print(ldrValues);
+      return value;
+    });
+  }
+
   Widget build(BuildContext context) {
-    return Center(child: Text('This is the statistics screen'));
+    return Padding(
+        padding: const EdgeInsets.all(15),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              "Brightness Readings",
+              style: TextStyle(fontSize: 25),
+            ),
+            SizedBox(
+              height: 50,
+            ),
+            SingleChildScrollView(
+              scrollDirection: Axis.horizontal,
+              reverse: true,
+              child: LineGraph(
+                features: [
+                  Feature(title: "Sensor", color: Colors.blue, data: [
+                    0.1,
+                    0.2,
+                    0.6,
+                    0.4,
+                    0.3,
+                    0.6,
+                    0.7,
+                    0.2,
+                    0.9,
+                    1.0
+                  ] //List.from(ldrData.reversed),
+                      ),
+                ],
+                size: Size(
+                    380, 400), //Size((ldrData.length * 70).toDouble(), 400),
+                labelX: const [
+                  '1',
+                  '2',
+                  '3',
+                  '4',
+                  '5',
+                  '6',
+                  '7',
+                  '8',
+                  '9',
+                  '10'
+                ], //List.from(time.reversed),
+                labelY: ['20%', '40%', '60%', '80%', '100%'],
+                graphColor: Colors.blue,
+                graphOpacity: 0.2,
+              ),
+            )
+          ],
+        ));
   }
 }
